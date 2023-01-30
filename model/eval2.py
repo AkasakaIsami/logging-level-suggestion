9cb0c6c576ee3f9878b61b5dd158507e4c95c4e1
AkasakaIsami <akasaka0623@163.com>
2023-01-31 07:47:53 +0800

    完成实验2

--- model/eval2.py
+++ model/eval2.py
@@ -4,112 +4,17 @@ import os
 import numpy as np
 import pandas as pd
 import torch
-from matplotlib import pyplot as plt
-from sklearn import manifold
-from sklearn.metrics import accuracy_score
-from torch import nn
-from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
+
+from sklearn.metrics import accuracy_score, roc_auc_score
 from torch.utils.data import random_split, DataLoader
 
 from dataset import MyDataset
-from util import float_to_percent
+from eval0 import MyLSTM
+from util import float_to_percent, transact, OR2OEN, AOD, visual, tensor2label, class_acc
 
 """
 完成实验2：AST pooling + RNN (只到当前语句)
 """
-
-class MyLSTM(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-        self.num_layers = 1
-        self.num_directions = 1  # 单向LSTM
-        self.hidden_size = 64
-
-        self.lstm = nn.LSTM(input_size=128, hidden_size=64, num_layers=self.num_layers, batch_first=True)
-        self.linear = nn.Linear(64, 5)
-        self.act = nn.Sigmoid()
-
-    def forward(self, x):
-        batch_size, seq_len = x.shape[0], x.shape[1]
-
-        # 首先对x进行压缩
-        # x = pack_padded_sequence(x, x.shape[0], batch_first=True)
-
-        h_0 = torch.randn(self.num_directions * self.num_layers, batch_size, self.hidden_size).requires_grad_().to(
-            self.device)
-        c_0 = torch.randn(self.num_directions * self.num_layers, batch_size, self.hidden_size).requires_grad_().to(
-            self.device)
-
-        output, _ = self.lstm(x, (h_0.detach(), c_0.detach()))
-
-        # 过完lstm解压
-        # output, _ = pad_packed_sequence(output, batch_first=True)
-
-        # 解压完后过线性层，再根据index取出该batch内所有的回归结果
-
-        pred = self.linear(output[:, -1, :])
-        pred = self.act(pred)
-        return output[:, -1, :], pred
-
-
-def idx2index(idx: torch.Tensor) -> torch.Tensor:
-    """
-    根据稀疏矩阵求index
-    """
-    index = []
-    size = idx.shape[0]
-    for i in range(size):
-        if idx[i].item() == 1:
-            index.append(i)
-    return torch.tensor(index).long()
-
-
-def visual(x, y, epoch):
-    # t-SNE的最终结果的降维与可视化
-    tsne = manifold.TSNE(n_components=2, init='pca', random_state=0, )
-    x = tsne.fit_transform(x)
-    x_min, x_max = np.min(x, 0), np.max(x, 0)
-    x = (x - x_min) / (x_max - x_min)
-
-    plt.figure(figsize=(10, 5))
-    plt.subplot(121)
-    plt.scatter(x[:, 0], x[:, 1], c=y, label="t-SNE")
-    plt.legend()
-
-    # for i in range(x.shape[0]):
-    #     plt.text(x[i, 0],
-    #              x[i, 1],
-    #              str(y[i]),
-    #              color=plt.cm.Set1(y[i]))
-
-    # plt.xticks([])  # 去掉横坐标值
-    # plt.yticks([])  # 去掉纵坐标值
-    f = plt.gcf()  # 获取当前图像
-    if epoch == -1:
-        f.savefig(f'./result/test.png')
-    else:
-        f.savefig(f'./result/{epoch}.png')
-
-    f.clear()  # 释放内存
-    plt.show()
-
-
-def transact(tensor: torch.Tensor) -> torch.Tensor:
-    """
-    把预测值转换成ordinal vector
-    """
-    size = tensor.shape[0]
-    result = torch.zeros(size, 5)
-    for i in range(size):
-        for j in range(5):
-            if tensor[i][j] > 0.5:
-                result[i][j] = 1
-            else:
-                break
-    return result
-
 
 if __name__ == '__main__':
     """
@@ -143,6 +48,8 @@ if __name__ == '__main__':
     train_dataset, val_dataset, test_dataset = random_split(dataset=dataset,
                                                             lengths=[train_len, val_len, test_len],
                                                             generator=torch.Generator().manual_seed(0))
+    print(
+        f"数据集切分完成，总共{len(dataset)}条数据，其中训练集{len(train_dataset)}条，验证集{len(val_dataset)}条，测试集{len(test_dataset)}条，")
 
 
     # 第四步 定义数据获取batch格式
@@ -150,6 +57,7 @@ if __name__ == '__main__':
         xs = []
         ys = []
         ids = []
+        max_len = 0
 
         for data in batch:
             method = data.id
@@ -171,7 +79,18 @@ if __name__ == '__main__':
             xs.append(seq)
             ys.append(y)
             ids.append(data.id + '@' + str(data.line.item()))
-        xs = pad_sequence(xs, batch_first=True)
+            max_len = seq.shape[0] if seq.shape[0] > max_len else max_len
+
+        # xs需要在前面补0
+        # 先找到最长的长度
+        for i in range(len(xs)):
+            seq = xs[i]
+            length = seq.shape[0]
+            for j in range(max_len - length):
+                xs[i] = torch.cat([torch.zeros(1, 128), xs[i]], dim=0)
+
+        # 补完以后把所有的拼起来
+        xs = torch.stack([x for x in xs], dim=0)
         ys = torch.cat([y for y in ys], dim=0).float()
 
         return xs, ys, ids
@@ -229,8 +148,8 @@ if __name__ == '__main__':
 
         # 验证
         total_val_loss = 0.0
-        y_hat_total = torch.randn(0).to(device)
-        y_total = torch.randn(0).to(device)
+        y_hat_total = torch.randn(0, 5)
+        y_total = torch.randn(0, 5)
 
         xs = torch.randn(0, 64)
         ys = []
@@ -244,13 +163,9 @@ if __name__ == '__main__':
 
                 # 用来计算整体指标
                 total_val_loss += loss.item()
-
                 y_hat = transact(y_hat).to(device)
-                print(y_hat)
-                print(y)
-
-                y_hat_total = torch.cat([y_hat_total, y_hat])
-                y_total = torch.cat([y_total, y])
+                y_hat_total = torch.cat([y_hat_total, OR2OEN(y_hat)], dim=0)
+                y_total = torch.cat([y_total, OR2OEN(y)], dim=0)
 
                 # 根据实际lable
                 size = len(y)
@@ -273,10 +188,14 @@ if __name__ == '__main__':
         print(f"验证集整体Loss: {total_val_loss}")
 
         acc = accuracy_score(y_total.cpu(), y_hat_total.cpu())
+        auc = roc_auc_score(y_total.cpu(), y_hat_total.cpu())
+        aod = AOD(y_total.cpu(), y_hat_total.cpu())
         print(f"验证集 accuracy_score: {float_to_percent(acc)}")
+        print(f"验证集 auc: {float_to_percent(auc)}")
+        print(f"验证集 aod: {float_to_percent(aod)}")
 
         if acc > best_acc:
-            print(f"***当前模型的平衡准确率表现最好，被记为表现最好的模型***\n")
+            print(f"***当前模型的准确率表现最好，被记为表现最好的模型***\n")
             best_model = model
             best_acc = acc
 
@@ -286,3 +205,94 @@ if __name__ == '__main__':
         ys = np.array(ys)
 
         visual(xs, ys, epoch + 1)
+
+    # ————————————————————————————————————————————————————————————————————————————————————————————————
+    # 测试集
+
+    correct = {}
+    wrong = {}
+
+    total_val_loss = 0.0
+    y_hat_total = torch.randn(0, 5).to(device)
+    y_total = torch.randn(0, 5).to(device)
+
+    xs = torch.randn(0, 64)
+    ys = []
+
+    record_file = open(os.path.join('./', 'result', 'result.txt'), 'w')
+
+    best_model.eval()
+    with torch.no_grad():
+        for i, (x, y, ids) in enumerate(test_loader):
+            h, y_hat = best_model(x.to(device))
+            y = y.to(device)
+            loss = loss_function(y_hat, y)
+
+            # 用来计算整体指标
+            total_val_loss += loss.item()
+            y_hat = transact(y_hat).to(device)
+            y_hat_total = torch.cat([y_hat_total, OR2OEN(y_hat)], dim=0)
+            y_total = torch.cat([y_total, OR2OEN(y)], dim=0)
+
+            # 这里帮助可视化
+            size = len(y)
+            for j in range(size):
+                label = torch.index_select(y.cpu(), dim=0, index=torch.tensor([j]))
+                temph = torch.index_select(h.cpu(), dim=0, index=torch.tensor([j]))
+                xs = torch.cat([xs, temph], dim=0)
+                if torch.equal(label, torch.tensor([[1, 1, 1, 1, 1]]).float()):
+                    ys.append(4)
+                elif torch.equal(label, torch.tensor([[1, 1, 1, 1, 0]]).float()):
+                    ys.append(3)
+                elif torch.equal(label, torch.tensor([[1, 1, 1, 0, 0]]).float()):
+                    ys.append(2)
+                elif torch.equal(label, torch.tensor([[1, 1, 0, 0, 0]]).float()):
+                    ys.append(1)
+                elif torch.equal(label, torch.tensor([[1, 0, 0, 0, 0]]).float()):
+                    ys.append(0)
+
+            # 我们记录所有预测错误的语句
+            for j in range(size):
+                fac = y.cpu()[j]
+                pre = y_hat.cpu()[j]
+                id = ids[j]
+
+                if torch.equal(fac, pre):
+                    correct[id] = tensor2label(fac)
+                else:
+                    wrong[id] = 'expecting ' + tensor2label(fac) + ' but got ' + tensor2label(pre)
+
+    print(f"测试集整体Loss: {total_val_loss}")
+
+    acc = accuracy_score(y_total.cpu(), y_hat_total.cpu())
+    auc = roc_auc_score(y_total.cpu(), y_hat_total.cpu())
+    class_acc = class_acc(y_total.cpu(), y_hat_total.cpu())
+    aod = AOD(y_total.cpu(), y_hat_total.cpu())
+    print(f"测试集 accuracy_score: {float_to_percent(acc)}")
+    print(f"测试集 auc: {float_to_percent(auc)}")
+    print(f"测试集 aod: {float_to_percent(aod)}")
+    print(f"测试集 各类别准确率: {class_acc}")
+
+    print(f"***保存tsne中***\n")
+
+    xs = xs.numpy()
+    ys = np.array(ys)
+
+    visual(xs, ys, -1)
+
+    print(f"***写入测试集测试结果中***\n")
+    record_file.write('预测正确的:\n')
+    num = 0
+    for key, value in correct.items():
+        record_file.write(f"    -{num}. {key} : {value}\n")
+        num += 1
+
+    record_file.write('——————————————————————————————————————————————————————\n')
+
+    record_file.write('预测错误的:\n')
+    num = 0
+    for key, value in wrong.items():
+        record_file.write(f"    -{num}. {key} : {value}\n")
+        num += 1
+
+    record_file.close()
